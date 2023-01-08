8a68a22599857a446ddda43a0d20ee9d038f2718
Sherlock <fhawaa@gmail.com>
2023-01-08 11:14:30 -0800

    Add files via upload

---
+++ SampleCheatWithoutBaseAddress.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    int Of_Health = 0x0;//offest
+    int Ad_Health = 0x0;//Address
+    int wr_Hleath = 1337;
+    DWORD health = (DWORD)(Ad_Health + Of_Health);
+    HWND hwnd = FindWindowA(NULL, "NameWindowGame");
+    if (hwnd == NULL)
+    {
+        cout << "Cannot Find Game !";
+        Sleep(300);
+        exit(-1);
+    }
+    else
+    {
+        DWORD procID;
+        GetWindowThreadProcessId(hwnd, &procID);
+        HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
+        if (procID == NULL)
+        {
+            cout << "Cannot obtain Process !";
+            Sleep(300);
+            exit(-1);
+        }
+        else
+        {
+            for (;;)
+            {
+                WriteProcessMemory(handle, (PBYTE*)health, &wr_Hleath, sizeof(wr_Hleath), 0);
+                Sleep(1000);
+            }
+        }
+
+    }
+
+
+}
